5e59103aa688cc6651814bb9897e7573ef783c78
Jeff Hykin <jeff.hykin@gmail.com>
2023-01-30 13:22:06 -0600

    -

--- distros/noetic/polled-camera/default.nix
+++ distros/noetic/polled-camera/default.nix
@@ -10,8 +10,10 @@ buildRosPackage {
   src = fetchurl {
     url = "https://github.com/ros-gbp/image_common-release/archive/97b80c9dfb3b3d198082b17beb25d99bcdfc01f7.tar.gz";
     name = "1.12.0-1.tar.gz";
-    sha256 = "35c1b2ff1b29f127d621eb459b0f85ee31d916843bb9c9557c1ad9eaf650aa3d"; # 9g195MQsPHHCpV+u/K2p0jsTUYu88+Awba0kmU0aJ98
+    sha256 = "9g195MQsPHHCpV+u/K2p0jsTUYu88+Awba0kmU0aJ98"; # 9g195MQsPHHCpV+u/K2p0jsTUYu88+Awba0kmU0aJ98
   };
+    #   specified: sha256-NcGy/xsp8SfWIetFmw+F7jHZFoQ7uclVfBrZ6vZQqj0=
+        # got:     sha256-9g195MQsPHHCpV+u/K2p0jsTUYu88+Awba0kmU0aJ98=
 
   buildType = "catkin";
   buildInputs = [ catkin message-generation ];
