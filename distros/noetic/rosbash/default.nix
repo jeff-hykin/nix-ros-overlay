ce8c6459503b5440f2a64379753f3e7ed107b201
Jeff Hykin <jeff.hykin@gmail.com>
2023-01-30 17:10:20 -0600

    -

--- distros/noetic/rosbash/default.nix
+++ distros/noetic/rosbash/default.nix
@@ -11,7 +11,7 @@ buildRosPackage {
     # https://github.com/ros-gbp/ros-release/
     url = "https://github.com/ros-gbp/ros-release/archive/bda9f415771aa41dddd81bfaa8e31a2c038524ef.tar.gz";
     name = "1.15.8-1.tar.gz";
-    sha256 = "sha256-muuWgpFIHK4m69/ZxW/kIp7lqsizip+8nN3ZaXeQR8Y=";
+    sha256 = "sha256-hYg5AE15i4XUL85FMdMyTzh9HBXtUK2LAJmLABo3N/c=";
   };
 
   buildType = "catkin";
